f9056ab291fe5d3c28a8fcb206df106c7f420051
harsh <rawalharsh48@gmail.com>
2020-04-21 00:54:57 -0400

    ToolKit page redirection and initialization

---
+++ components/toolKit.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View , ImageBackground, TouchableOpacity } from 'react-native';
+
+export default class toolKit extends Component{
+    render(){
+        return(
+            <Text>ToolKit page</Text>
+        )
+    }
+}
