af95d58a154d59f07d062d8c8c93d0a356671eb4
harsh <rawalharsh48@gmail.com>
2020-04-22 00:03:23 -0400

    Added functionality for logining In using Facebook

--- components/homescreen.js
+++ components/homescreen.js
@@ -1,41 +1,79 @@
 import React, { Component } from 'react';
-import { Button , StyleSheet, Text, View , ImageBackground, TouchableOpacity } from 'react-native';
+import {  StyleSheet, Text, View , ImageBackground, TouchableOpacity , Image } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import * as Google from "expo-google-app-auth";
+import * as Facebook from 'expo-facebook';
+
+
+
 export default class homescreen extends Component {
    
   constructor(props) {
     super(props)
     this.state = {
       signedIn: false,
-      name: "",
-      photoUrl: ""
     }
   }
 
 
   render() {
+    
     signInWithGoogle = async () => {
-      try {
-        const result = await Google.logInAsync({
-          iosClientId: "310530671505-sstrq6kalprkbn0ldrl5fmd7c47oaccf.apps.googleusercontent.com",
-          scopes: ["profile", "email"]
-        });
-  
-        if (result.type === "success") {
-          console.log("LoginScreen.js.js 21 | ", result.user.givenName);
-          this.props.navigation.navigate("second_page", {
-            username: result.user.givenName
-          }); //after Google login redirect to Profile
-          return result.accessToken;
-        } else {
-          return { cancelled: true };
+      if(this.state.signedIn) 
+        this.props.navigation.navigate("second_page");
+      else{
+        try {
+          const result = await Google.logInAsync({
+            iosClientId: "310530671505-sstrq6kalprkbn0ldrl5fmd7c47oaccf.apps.googleusercontent.com",
+            scopes: ["profile", "email"]
+          });
+    
+          if (result.type === "success") {
+            this.setState({signedIn : true});
+            this.props.navigation.navigate("second_page", {
+              username: result.user.givenName
+            });
+            return result.accessToken;
+          } else {
+            return { cancelled: true };
+          }
+        } catch (e) {
+          return { error: true };
         }
-      } catch (e) {
-        console.log('LoginScreen.js.js 30 | Error with login', e);
-        return { error: true };
-      }
+    }
     };
+
+     logInWithFacebook = async() => {
+      if(this.state.signedIn)
+        this.props.navigation.navigate("second_page");
+      else{
+        try {
+          
+          await Facebook.initializeAsync('2376431252655014');
+          const {
+            type,
+            token,
+            expires,
+            permissions,
+            declinedPermissions,
+          } = await Facebook.logInWithReadPermissionsAsync({
+            permissions: ['public_profile'],
+          });
+          
+          if (type === 'success') {
+            this.setState({signedIn : true});
+            const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+            this.props.navigation.navigate("second_page", {
+              username: `${((await response.json()).name).split(" ")[0]}`
+            });
+          } else {
+            // type === 'cancel'
+          }
+        } catch ({ message }) {
+          alert(`Facebook Login Error: ${message}`);
+        }
+    }
+  }
     return (
         <View style={styles.container}>
           <ImageBackground 
@@ -45,36 +83,47 @@ export default class homescreen extends Component {
             <View style = {styles.container} >
               <Text
               style = {{
-                fontSize : 70 ,
+                fontSize : 100 ,
                 fontFamily: 'AvenirNextCondensed-DemiBold',
                 color :'white',
                 flexWrap:'wrap',
-                bottom : 250,
+                bottom : 230,
+                left: -5
                 }}> 
                         Serenus
               </Text>
               <Text
               style = {{
-                fontSize : 27 ,
+                fontSize : 35 ,
                 fontFamily: 'AvenirNextCondensed-DemiBold',
                 color :'white',
-                bottom : 258,
-                justifyContent : 'center', 
+                bottom : 228,
+                justifyContent : 'center',
+                left :9
                 }}> 
                           Making mental health a  normal conversation
               </Text>
+              <TouchableOpacity 
+                style = {styles.Buttons}
+                onPress={() => signInWithGoogle()}
+               >
+                 <Image source={require('../images/google.png')} style ={{width : 30 , height : 30 , left :-110 , bottom : -10}}></Image>
+                 <Text style ={{color :'white' , fontSize :20 , bottom : 15}}>
+                   Sign in with Google
+                 </Text>
+               </TouchableOpacity>
 
-              <Button title="Sign in with Google" 
-              onPress={() => signInWithGoogle()}
-               />
-            <TouchableOpacity style = {styles.Buttons}
-                onPress={() => this.props.navigation.navigate('second_page')}
-                >
-              <Text style ={{color : 'white' ,fontSize : 25 ,fontFamily: 'AvenirNext-Bold'}}> Let's get Started </Text>
-            </TouchableOpacity>
+               <TouchableOpacity 
+                style = {styles.Buttons_2}
+                onPress={() => logInWithFacebook()}
+               >
+                 <Image source={require('../images/google.png')} style ={{width : 30 , height : 30 , left :-110 , bottom : -5}}></Image>
+                 <Text style ={{color :'white' , fontSize :20 , bottom : 20}}>
+                   Sign in with facebook
+                 </Text>
+               </TouchableOpacity>
           </View>
         </ImageBackground>
-  
       </View>
     )
   }
@@ -99,7 +148,7 @@ const styles = StyleSheet.create({
     left: 10,
   },
   Buttons:{
-    width: '70%', 
+    width: '80%', 
     height: 70, 
     color : 'white',
     borderColor:'white', 
@@ -110,11 +159,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center', 
     alignItems: 'center',
     position: 'absolute',
-    bottom: 30,
     borderTopRightRadius: 40,
     borderTopLeftRadius: 40,
     borderBottomRightRadius: 40,
     borderBottomLeftRadius: 40,
+    bottom : 0
+  },
+  Buttons_2:{
+    width: '80%', 
+    height: 70, 
+    color : 'white',
+    borderColor:'white', 
+    borderBottomWidth:2,
+    borderTopWidth:2,
+    borderLeftWidth:2,
+    borderRightWidth:2,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    position: 'absolute',
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    borderBottomLeftRadius: 40,
+    bottom : 90
   }, 
   text_box:{
 
