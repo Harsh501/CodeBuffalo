06966b5de7da7f7c91866404a788fe4efa5f85e9
harsh <rawalharsh48@gmail.com>
2020-04-23 00:51:31 -0400

    Added skeleton files for analytics page and panic page

---
+++ components/analytics.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import {  StyleSheet, Text, View , ImageBackground, TouchableOpacity , Image } from 'react-native';
+
+export default class analytics extends Component{
+    render(){
+        return <Text>At Analytics!</Text>
+    }
+}
---
+++ components/emergency.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import {  StyleSheet, Text, View , ImageBackground, TouchableOpacity , Image } from 'react-native';
+
+export default class emergency extends Component{
+    render(){
+        return <Text>At Alert!</Text>
+    }
+}
