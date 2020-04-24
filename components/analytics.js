35fd1eb9b0011e3f963e4de8301fdd543cbc8088
harsh <rawalharsh48@gmail.com>
2020-04-24 02:45:11 -0400

    Moving files

--- components/analytics.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import {  StyleSheet, Text, View , ImageBackground, TouchableOpacity , Image } from 'react-native';
-
-export default class analytics extends Component{
-    render(){
-        return <Text>At Analytics!</Text>
-    }
-}
---
+++ components/day.js
@@ -0,0 +1,240 @@
+import React, { Component } from 'react';
+import {
+  Switch,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import Constants from 'expo-constants';
+import * as Animatable from 'react-native-animatable';
+import Collapsible from 'react-native-collapsible';
+import Accordion from 'react-native-collapsible/Accordion';
+
+const BACON_IPSUM =
+  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
+
+const CONTENT = [
+  {
+    title: 'Sat, April 18                                              2',
+    content: <Image
+    source={require("../images/happy_logo.jpeg")}
+    style={{height : 80 , width : 80 , left :7}}
+    />
+  },
+  {
+    title: 'Sun, April 19                                             1',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Mon, April 20                                           3',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Tue, April 21                                             1',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Wed, April 22                                           2',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Thu, April 23                                            1',
+    content: BACON_IPSUM,
+  },
+];
+// Day week year
+const SELECTORS = [
+  {
+    title: 'First',
+    value: 0,
+  },
+  {
+    title: 'Third',
+    value: 2,
+  },
+  {
+    title: 'None',
+  },
+];
+
+export default class day extends Component {
+  state = {
+    activeSections: [],
+    collapsed: true,
+    multipleSelect: false,
+  };
+
+  toggleExpanded = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  setSections = sections => {
+    this.setState({
+      activeSections: sections.includes(undefined) ? [] : sections,
+    });
+  };
+
+  renderHeader = (section, _, isActive) => {
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor"
+      >
+        <Text style={styles.headerText}>{section.title}</Text>
+      </Animatable.View>
+    );
+  };
+
+  renderContent(section, _, isActive) {
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor"
+      >
+        <Animatable.Text animation={isActive ? 'bounceIn' : undefined}>
+          {section.content}
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  }
+
+  render() {
+    const { multipleSelect, activeSections } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
+          <View style={styles.selectors}>
+            
+
+            {SELECTORS.map(selector => (
+                
+              <TouchableOpacity
+                key={selector.title}
+                onPress={() => this.setSections([selector.value])}
+              >
+                <View style={styles.selector}>
+                  <Text
+                    style={
+                      activeSections.includes(selector.value) &&
+                      styles.activeSelector
+                    }
+                    style= {{fontSize : 22}}
+                  >
+                    {selector.title} |
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+
+          
+            <View style={styles.header}>
+              <Text style={styles.headerText}>Date</Text>
+              <Text style={styles.headerText_1}>Check in's</Text>
+            
+
+            </View>
+          
+          {/* <Collapsible collapsed={this.state.collapsed} align="center"/> */}
+          <Accordion
+            activeSections={activeSections}
+            sections={CONTENT}
+            touchableComponent={TouchableOpacity}
+            expandMultiple={multipleSelect}
+            renderHeader={this.renderHeader}
+            renderContent={this.renderContent}
+            duration={400}
+            onChange={this.setSections}
+          />
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    paddingTop: Constants.statusBarHeight,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: '#F5FCFF',
+    padding: 25,
+    borderBottomWidth : 3,
+    borderLeftWidth : 3,
+    borderRightWidth : 3,
+    
+    borderColor : 'black',
+    justifyContent : 'center',
+    borderRadius : 20
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 19,
+    fontWeight: '500',
+    left : 10,
+    position : 'absolute',
+  },
+  headerText_1: {
+    justifyContent : 'center',
+    textAlign: 'center',
+    fontSize: 19,
+    fontWeight: '500',
+    left : 104,
+    top : 3
+  },
+  content: {
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
+  },
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    left : 78,
+    top : -10
+  },
+  selector: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: 'bold',
+    
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10,
+    left : 10
+  },
+  multipleToggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+    alignItems: 'center',
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+});
